0680e603b958ee33efebde603da66c16e93eb233
holylong <707010543@qq.com>
2022-01-28 13:35:09 +0800

    add superjumper

---
+++ superjumper/android/src/com/badlogicgames/superjumper/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.badlogicgames.superjumper.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogicgames.superjumper.SuperJumper;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;
+        config.useWakelock = true;
+		initialize(new SuperJumper(), config);
+	}
+}
