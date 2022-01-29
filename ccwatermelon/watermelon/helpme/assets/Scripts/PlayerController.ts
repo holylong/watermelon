9f87378835dfe2a859bacf463f9154ff070df8d4
holylong <707010543@qq.com>
2022-01-29 12:39:57 +0800

    add animation

--- ccwatermelon/watermelon/helpme/assets/Scripts/PlayerController.ts
+++ ccwatermelon/watermelon/helpme/assets/Scripts/PlayerController.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Vec3, systemEvent, SystemEvent, EventMouse, log } from 'cc';
 const { ccclass, property } = _decorator;
 
 /**
@@ -28,14 +28,75 @@ export class PlayerController extends Component {
     private _jumpStep:number = 0;
     //当前跳跃时间
     private _curJumpTime:number = 0;
+    //每次跳跃时长
+    private _jumpTime:number = 0.1;
+    //当前跳跃速度
+    private _curJumpSpeed:number = 0;
+    //当前角色位置
+    private _curPos:Vec3 = new Vec3();
+    //每次跳跃过程中，当前帧移动位置差
+    private _deltaPos:Vec3 = new Vec3(0,0,0);
+    //角色位置目标
+    private _targetPos:Vec3 = new Vec3();
+
+    @property({type: Animation})
+    public BodyAnim: Animation | null = null;
 
     start () {
-        // [3]
+        console.log("==>> start");
+        systemEvent.on(SystemEvent.EventType.MOUSE_UP,this.onMouseUp, this);
     }
 
-    // update (deltaTime: number) {
-    //     // [4]
-    // }
+    onMouseUp(event:EventMouse){
+        console.log('==>> mouse up');
+        if(event.getButton() == 0){
+            this.jumpByStep(1);
+        }else if(event.getButton() == 2){
+            this.jumpByStep(2);
+        }
+    }
+
+    jumpByStep(step:number){
+        console.log('==>> jump step', step);
+        if(this._startJump){
+            return;
+        }
+        if (this.BodyAnim) {
+            if (step === 1) {
+                console.log("==>animation")
+                this.BodyAnim.play('oneStep');
+            } else if (step === 2) {
+                //this.BodyAnim.play('twoStep');
+                console.log("==>> 2 step animation")
+            }
+        }else{
+            console.log("==>> body animation is null")
+        }
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+        this._curJumpSpeed = this._jumpStep/this._jumpTime;
+        this.node.getPosition(this._curPos);
+        Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep,0,0));
+    }
+
+    update (deltaTime: number) {
+        // console.log('==>> update');
+        if(this._startJump){
+            this._curJumpTime += deltaTime;
+            if(this._curJumpTime > this._jumpTime){
+                //end
+                this.node.setPosition(this._targetPos);
+                this._startJump = false;
+            }else{
+                //tween
+                this.node.getPosition(this._curPos);
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                Vec3.add(this._curPos, this._curPos, this._deltaPos);
+                this.node.setPosition(this._curPos);
+            }
+        }
+    }
 }
 
 /**
